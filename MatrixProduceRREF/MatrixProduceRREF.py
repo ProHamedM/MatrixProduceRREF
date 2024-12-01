80dd39e814112e843b953b2f1acee1aa34967ff4
Hamed <hamed.official2019@gmail.com>
2024-12-01 19:11:54 +0330

    temp test

--- MatrixProduceRREF/MatrixProduceRREF.py
+++ MatrixProduceRREF/MatrixProduceRREF.py
@@ -1,7 +1,24 @@
 
-# Ask client path & sheet
-path = input ('Please enter excel File\'s Path & Sheet?')
-print (path)
+
+# Ask client path
+#path = input ('Please enter excel File\'s Path?')
+#print (path)
+
+from pandas import pd
+
 
 def produceMatrix ():
-    return
+    mydataset = {
+    'cars': ["BMW", "Volvo", "Ford"],
+    'passings': [3, 7, 2]}
+    myvar = pd.DataFrame(mydataset)
+    print(myvar)
+
+
+
+
+
+    #doc = pd.DataFrame(pd.read_excel(path))
+    
+
+  #print (doc)
